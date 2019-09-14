28baebd78f9a1a6620b85d4415ea79c517f8e013
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-09-14 00:32:00 -0400

    added homepage.component.jsx which holds the homepage component. App.js: imported HomePage component and rendered it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./Components/homepage.component";
 
-function App() {
-  return (
-    <div>Hello World!</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
