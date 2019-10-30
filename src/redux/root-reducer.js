712473aa610fdd6a321279339c3c837379edfd23
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-10-29 23:50:18 -0400

    Completed setup of redux-persist. This will save the state in local storage.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from "./cart/cart.reducer";
+import {persistReducer} from 'redux-persist';
 
-export default combineReducers({
+//import type of persist storage store: (important to use the module name as below)
+import storage from 'redux-persist/lib/storage'; /*localStorage*/
+// import sessionStorage from 'redux-persist/lib/storage/session'; /*sessionStorage*/
+
+// jsonObject that represents the possible configurations that we want for redux-persist to use
+const persistConfig = {
+  key: 'root',
+  storage: storage, /*or sessionStorage*/
+  whitelist: ['cart']  /*add the reducers you want to save in local or session Storage as strings*/
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
