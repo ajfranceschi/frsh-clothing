03ce48de79ab491bfcc460813af18a36441e8707
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-11-19 16:55:43 -0500

    changed shop_data to be an object instead of an array.

--- src/Pages/Collection/Collection.component.jsx
+++ src/Pages/Collection/Collection.component.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import './Collection.styles.sass';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import CollectionItem from './../../Components/CollectionItem/collectionItem.component';
-import {selectCollection} from '../../redux/shop/shop.selectors';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
-const CollectionPage = ({collection}) => {
-
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
     return (
-        <div className="category">
-            <h2>{`${collection.title.toUpperCase()}`}</h2>
+        <div className='collectionPage'>
+            <h2 className='title'>{`${title}`}</h2>
+            <div className='items'>
+                {items.map(item => {
+                    return <CollectionItem key={item.id} item={item} />;
+                })}
+            </div>
         </div>
-    )
+    );
 };
 
 const mapStateToProps = (state, ownProps) => {
     return {
         collection: selectCollection(ownProps.match.params.collectionId)(state)
-    }
+    };
 };
 
 export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
 
 // make selector input
 const selectShop = rootReducerState => rootReducerState.shop;
@@ -21,5 +14,5 @@ export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectShopCollections],
         collections =>
-            collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+            collections[collectionUrlParam]
     );
