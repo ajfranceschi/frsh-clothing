41b86aee4a4c7fda59679228ff151a92eff3da1e
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-11-22 00:45:22 -0500

    Integrated Stripe and updated npm packages

---
+++ src/Components/StripeButton/stripeButton.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_QQ1yAqP2NnkvykBhKdnHoHNb00V8X1GE4D';
+
+    const onToken = token => {
+        // here's what you would send to the back end to process the payment
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='FRSH Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
--- src/Pages/Checkout/checkout.component.jsx
+++ src/Pages/Checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../Components/CheckoutItem/checkoutItem.component';
+import StripeCheckoutButton from '../../Components/StripeButton/stripeButton.component';
 
 const CheckoutPage = ({ cartItems, total }) => {
     return (
@@ -32,6 +33,12 @@ const CheckoutPage = ({ cartItems, total }) => {
             <div className='total'>
                 <span>TOTAL: ${total}</span>
             </div>
+            <div className="testWarning">
+                * Please use the following test credit card for payments*
+                <br/>
+                4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={total}/>
         </div>
     );
 };
