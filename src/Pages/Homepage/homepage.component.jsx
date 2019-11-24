00fda1e648130d13cd25c9be2e96a675ea6a1027
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-11-24 01:16:21 -0500

    installed styled-components.  Changed HomePage to use Styled-Components

--- src/Pages/Homepage/homepage.component.jsx
+++ src/Pages/Homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './homepage.styles.sass'
 import Directory from "../../Components/Directory/directory.component";
-
+import {HomePageContainer} from './homepage.styles';
 
 const HomePage = () => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory   />
-    </div>
+    </HomePageContainer>
   )
 };
 
