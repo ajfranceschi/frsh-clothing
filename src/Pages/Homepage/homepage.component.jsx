f1f975aacb26ade84543eaa41c90100be0ee9ef1
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-11-24 01:19:09 -0500

    removed sass file import form homepage.component.jsx / added header.styles.jsx

--- src/Pages/Homepage/homepage.component.jsx
+++ src/Pages/Homepage/homepage.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './homepage.styles.sass'
 import Directory from "../../Components/Directory/directory.component";
 import {HomePageContainer} from './homepage.styles';
 
