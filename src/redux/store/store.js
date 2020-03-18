b1bc6f79c2f0a1027a1dd4c4a549faf0c14215b4
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-03-18 01:44:47 -0400

    removed Thunk and began sagas implementation

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,16 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from '../root-reducer';
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga';
+
+// create
+const sagaMiddleware = createSagaMiddleware();
 
 // applyMiddlewares() expects the middlewares,
-const middleWares = [thunk];
+const middleWares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middleWares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+//add individual sagas:
+// sagaMiddleware.run()
 
 export const persistor = persistStore(store);
 
